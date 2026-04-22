f6652e873227bbac86f7ea6bd0ef0eff3e84d425
StealLine <bogdan01ua@gmail.com>
2026-04-22 16:12:37 +0000

    Feature testminio pofig

--- backend/Libs/Infrastructure/InfrastructureShared.Storage/S3Config.cs
+++ backend/Libs/Infrastructure/InfrastructureShared.Storage/S3Config.cs
@@ -5,7 +5,8 @@ public class S3Config
     public string ServiceUrl { get; init; } = null!;
     public string AccessKey { get; init; } = null!;
     public string SecretKey { get; init; } = null!;
-    public S3MainBucketConf MainBucket { get; init; } = null!;
+	public bool ForcePathStyle { get; init; } = false; // added forced path style for minio
+	public S3MainBucketConf MainBucket { get; init; } = null!;
 }
 
 public sealed record class S3MainBucketConf(string Name);
--- backend/UserProfilesService/src/UserProfilesService.Infrastructure/DependencyInjection.cs
+++ backend/UserProfilesService/src/UserProfilesService.Infrastructure/DependencyInjection.cs
@@ -76,8 +76,12 @@ public static class DependencyInjection
 
         services.AddSingleton<IAmazonS3>(_ => new AmazonS3Client(
             new BasicAWSCredentials(s3Config.AccessKey, s3Config.SecretKey),
-            new AmazonS3Config { ServiceURL = s3Config.ServiceUrl }
-        ));
+	        new AmazonS3Config
+	        {
+		        ServiceURL = s3Config.ServiceUrl,
+		        ForcePathStyle = s3Config.ForcePathStyle
+	        }
+		));
 
         services.AddSingleton(s3Config.MainBucket); //S3MainBucketConf
         services.AddScoped<IMainStorageBucket, MainStorageBucket>();
